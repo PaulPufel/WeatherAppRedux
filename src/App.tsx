9ca9bc4fa680b8e669eebe3866b774cbd8354d55
PaulPufel <ppufel@web.de>
2025-01-30 15:05:52 +0100

    Resolve conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Layout from "./pages/Layout/Layout"
-<<<<<<< HEAD
 import Home from "./pages/Home/Home"
 import History from "./pages/History/History"
-=======
-import HomeW from "./pages/HomeW/HomeW"
-import HistoryW from "./pages/HistoryW/HistoryW"
->>>>>>> main
 
 const App = () => {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-<<<<<<< HEAD
           <Route path="/" element={<Home />} />
           <Route path="/history" element={<History />} />
           <Route path='*' element='Page not found'/>
-=======
-        {/* <Route path="/" element={<Home />} />
-        <Route path="/history" element={<History />} /> */}
-          <Route path="/" element={<HomeW />} />
-          <Route path="/history" element={<HistoryW />} />
->>>>>>> main
         </Routes>
       </Layout>
     </BrowserRouter>
